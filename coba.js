684f5a036170d4ab08aa80372beb5e5274a9511d
wardahasifa <96783505+wardahasifa@users.noreply.github.com>
2022-04-22 00:01:41 +0700

    Add files via upload

---
+++ coba.js
@@ -0,0 +1,23 @@
+const konten = {
+
+    articel1: 'Jurusan Sistem Informasi adalah bidang keilmuan yang menggabungkan ilmu komputer dengan bisnis dan manajemen.',
+    articel2: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    nav1 : 'Home',
+    nav2 : 'About',
+    nav3 : 'Information',
+    nav4 : 'Contact',
+    name : 'Wardah Asifa',
+    judul : 'PORTOFOLIO',
+    student : 'College Student | Information System',
+    isi1 : 'About Me',
+    item1 : "Formal Education",
+    item2 : 'Seminar',
+    item3 : 'Organization Experience',
+    item4 : 'Award',
+    item5 : 'Social Media',
+    item6 : 'Hobby'
+}
+const vm = new Vue({
+    el: ".bdy",
+    data: konten
+})
