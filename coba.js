813858f97a314955d563dddde164d186daedd6a4
wardahasifa <96783505+wardahasifa@users.noreply.github.com>
2022-07-27 21:28:30 +0700

    update API POINT

--- coba.js
+++ coba.js
@@ -1,6 +1,10 @@
-let app = Vue.createApp({
+const apiPoint = "https://myapi.wardahhh.my.id/index.php/";
+
+Vue.createApp({
     data() {
         return {
+            users: [],
+            portofolio: [],
             articles: {},
             markdown: null
         }
@@ -9,7 +13,7 @@ let app = Vue.createApp({
         getArticleData() {
             axios
                 .get(
-                    "https://raw.githubusercontent.com/wardahasifa/tekweb2022/main/data.json"
+                    apiPoint+"artikels"
                 )
                 .then((res) => {
                     this.articles = res.data;
@@ -18,10 +22,57 @@ let app = Vue.createApp({
                 .catch((error) => {
                     console.log(error);
                 });
+        },
+        getUserData() {
+            axios
+                .get(
+                    apiPoint+"users"
+                )
+                .then((res) => {
+                    this.users = res.data;
+                    console.log(res.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        getPortofolioData() {
+            axios
+                .get(
+                    apiPoint+"pors"
+                )
+                .then((res) => {
+                    this.portofolio = res.data;
+                    console.log(res.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        getMarkdownFile() {
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const fileName = urlParams.get('markdown');
+            var converter = new showdown.Converter();
+            console.log(fileName)
+            axios
+                .get(
+                    "https://raw.githubusercontent.com/wardahasifa/tekweb2022/main/" + fileName
+                )
+                .then((res) => {
+                    var html = converter.makeHtml(res.data);
+                    this.markdown = html;
+                    console.log(html)
+                })
+                .catch((error) => {
+                    console.log(error);
+                  });
         }
     },
     beforeMount() {
-        this.getArticleData()
-    }
-})
-app.mount('#app');
+        this.getArticleData(),
+        this.getUserData(),
+        this.getPortofolioData(),
+        this.getMarkdownFile()
+    },
+}).mount("#app");
